c5993c3bb8316d7651ccf873c35f309d42339541
denis-levkov <denis.levkov@zoominfo.com>
2024-07-21 19:44:57 +0300

    perfect 100% coverage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VerySimpleComponentComponent } from './very-simple-component/very-simple-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VerySimpleComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
